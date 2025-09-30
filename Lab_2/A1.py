c7da31840f8f81d9b46a5c441acbb9b561efc809
Ясинский Роман <ysinskij_rm_25@student.grsu.by>
2025-09-30 09:16:41 +0300

    Сделано задание A1 и добавлен файл задания A2

--- Lab_2/A1.py
+++ Lab_2/A1.py
@@ -1,4 +1,11 @@
-import sys
-a = int(sys.getsizeof(3 ** 9090001))
-mb = a/(1024 ** 2)
-print (mb)
+width = 17 # ширина
+height = 12.0 # высота
+a = width//2
+print (a)
+print(type (a))
+b = width/2.0
+print (b)
+print (type (b))
+c = height/3
+print (c)
+print (type (c))
