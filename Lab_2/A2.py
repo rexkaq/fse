e57fe3e772449b21d03e00d978fb9980edcb1628
Ясинский Роман <ysinskij_rm_25@student.grsu.by>
2025-09-30 09:32:28 +0300

    Сделано задание A2

---
+++ Lab_2/A2.py
@@ -0,0 +1,2 @@
+#Появляются ошибки ValueError: invalid literal for int() with base 10: ,OverflowError: (34, 'Result too large')
+print ("Нельзя преобразовать:'123е','91.4',524.345 ** 435345345311145345,'7.1 + 4','4' - 2,'4 - 2'")
